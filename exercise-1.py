ce0c57f059762b4a9f2df2aea12ac7b34ed08535
Mariia Rubel <mariiarubel@gmail.com>
2022-07-16 22:35:11 +0100

    Create exercise-1

---
+++ exercise-1.py
@@ -0,0 +1,17 @@
+
+# це варіант вирішення, до якого я додумалася сама
+x = 42
+if x % 2 == 0:
+    print(' The number is Even')
+else:
+    print('The number is Odd')
+
+
+# а це варіант вирішення, що я нагуглила
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+# обидва варіанта вважаю правильним вирішенням, але з інпутом цікавіше
